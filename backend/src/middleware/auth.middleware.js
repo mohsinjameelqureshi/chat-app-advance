10cc492a3d51dccdb9d1ba9f867a2bf5b7786ec5
Mohsin jameel <qureshimohsinjameel@gmail.com>
2025-10-05 21:36:53 +0500

    some minor changes resolved some issues

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -10,6 +10,10 @@ export const verifyJWT = asyncHandler(async (req, _, next) => {
     req?.body?.accessToken ||
     req?.headers?.authorization?.replace("Bearer ", "");
 
+  if (!token) {
+    throw new ApiError(401, "Access token is required");
+  }
+
   try {
     const decodeToken = jwt.verify(token, ENV.ACCESS_TOKEN_SECRET);
     const user = await User.findById(decodeToken?._id).select(
